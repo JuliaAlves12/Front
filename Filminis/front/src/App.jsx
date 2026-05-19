9859061358d0e3e0bf8ffb29b545ece7f394cb12
Julia Alves Pereira <Juliaalvespereira01@gmail.com>
2026-05-19 08:54:42 -0300

    fix: Arrumando rotas -- Login + LogOut(Não vê filmes)

--- Filminis/front/src/App.jsx
+++ Filminis/front/src/App.jsx
@@ -3,12 +3,28 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom" // Ferrame
 import "./App.css" // Estilo global do site
 import Movie from "./components/Movie/Movie" // Traz a página de detalhes
 import Login from "./components/Login/Login" // Traz a página de login
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
 
   const [token, setToken] = useState(null)
   const [role, setRole] = useState(null)
+
+  useEffect(() => {
+    const tokenSalvo = localStorage.getItem("access_token")
+    const roleSalvo = localStorage.getItem("user_role")
+
+    if (tokenSalvo && roleSalvo) {
+      setToken(tokenSalvo)
+      setRole(roleSalvo)
+    }
+  }, [])
+
+  const handleLogout = () => {
+    localStorage.clear()
+    setToken(null)
+    setRole(null)
+  }
 
   return (
     // 1. BrowserRouter: O "vigia". Ele fica olhando a barra de endereço do navegador.
@@ -22,17 +38,34 @@ function App() {
           <Link to="/login">
             <p>Login</p>
           </Link>
+
+          <button onClick={handleLogout}>Logout</button>
+
+
         </header>
         {/* 3. Routes: O "trocador de palcos". Só uma dessas rotas aparece por vez. */}
         <Routes>
-          {/* Se o endereço for "/" (vazio), mostre a vitrine (MovieList) */}
-          <Route path="/" element={<MovieList />} />
-          {/* Se o endereço tiver "/filme", mostre os detalhes (Movie) */}
-          <Route path="/filme" element={<Movie />} />
-          {/* Se o endereço tiver "/login", mostre o formulário (Login) */}
-          <Route path="/login" element={
-            <Login setRole={setRole} setToken={setToken} />} />
+
+          {token ? (
+            <>
+              {/* Se o endereço for "/" (vazio), mostre a vitrine (MovieList) */}
+              <Route path="/" element={<MovieList />} />
+              {/* Se o endereço tiver "/filme", mostre os detalhes (Movie) */}
+              <Route path="/filme" element={<Movie />} />
+              {/* Se o endereço tiver "/login", mostre o formulário (Login) */}
+              <Route path="/login" element={
+                <Login setRole={setRole} setToken={setToken} />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<MovieList />} />
+              <Route path="/login" element={
+                <Login setRole={setRole} setToken={setToken} />} />
+            </>
+          )}
+
         </Routes>
+
 
       </div>
     </BrowserRouter>
